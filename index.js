9da0727991206cb4d6e566634659fe020ee9676f
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-14 23:54:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name) {
-  let newDriversArr = drivers.slice().push(name);
+  let newDriversArr = drivers.slice();
+  newDriversArr.push(name);
   return newDriversArr;
 }
